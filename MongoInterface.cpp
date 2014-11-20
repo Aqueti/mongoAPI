1efdc2c1781417443fa53027ab2e530f3fdfddad
Cameron Givler <cdg26@duke.edu>
2014-11-20 13:16:56 -0500

    update

--- MongoInterface.cpp
+++ MongoInterface.cpp
@@ -11,81 +11,85 @@
 
 #include "MongoInterface.h"
 
-
 MongoInterface::MongoInterface(std::string database, std::string IP_Port) {
 	connection = new mongo::DBClientConnection();
 	connect(database, IP_Port);
 }
 
 MongoInterface::~MongoInterface() {
+	mongo::client::shutdown();
 	delete connection;
 }
 bool MongoInterface::connect(std::string database, std::string IP_Port) {
-    mongo::client::initialize();
-    try {
-        connection->connect(IP_Port);
-        std::cout << "connected ok" << std::endl;
-    } catch (const mongo::DBException &e) {
-        std::cout << "caught " << e.what() << std::endl;
-        return false;
-    }
-    this->IP_Port = IP_Port;
-    this->database = database;
-    return true;
+	mongo::client::initialize();
+	try {
+		connection->connect(IP_Port);
+		std::cout << "connected ok" << std::endl;
+	} catch (const mongo::DBException &e) {
+		std::cout << "caught " << e.what() << std::endl;
+		return false;
+	}
+	this->IP_Port = IP_Port;
+	this->database = database;
+	return true;
 }
 
 mongo::BSONObj MongoInterface::BSON_from_JSON(JsonBox::Value* data) const {
-    std::stringstream stream;
-    data->writeToStream(stream, false);
-    return mongo::fromjson(stream.str());
+	std::stringstream stream;
+	data->writeToStream(stream, false);
+	return mongo::fromjson(stream.str());
 }
 
 JsonBox::Value MongoInterface::JSON_from_BSON(mongo::BSONObj* data) const {
-    JsonBox::Value j;
-    j.loadFromString(data->jsonString());
-    return j;
+	JsonBox::Value j;
+	j.loadFromString(data->jsonString());
+	return j;
 }
 
-bool MongoInterface::insertJSON(std::string collection, JsonBox::Value* data) const {
-    try {
-        connection->insert(database + "." + collection, BSON_from_JSON(data));
-        return true;
-    } catch (const mongo::DBException& e) {
-        std::cout << "caught " << e.what() << std::endl;
-    }
-    return false;
+bool MongoInterface::insertJSON(std::string collection,
+		JsonBox::Value* data) const {
+	try {
+		connection->insert(database + "." + collection, BSON_from_JSON(data));
+		return true;
+	} catch (const mongo::DBException& e) {
+		std::cout << "caught " << e.what() << std::endl;
+	}
+	return false;
 }
 
-JsonBox::Value MongoInterface::query(std::string collection, JsonBox::Value* data) const {
-    JsonBox::Value results;
-    std::auto_ptr<mongo::DBClientCursor> cursor = connection->query(database + "." + collection, BSON_from_JSON(data));
-    int counter = 0;
-    while (cursor->more()) {
-        mongo::BSONObj b = cursor->next();
-        results[counter] = JSON_from_BSON(&b);
-        counter++;
-    }
-    results.writeToStream(std::cout);
-    return results;
+JsonBox::Value MongoInterface::query(std::string collection,
+		JsonBox::Value* data) const {
+	JsonBox::Value results;
+	std::auto_ptr<mongo::DBClientCursor> cursor = connection->query(
+			database + "." + collection, BSON_from_JSON(data));
+	int counter = 0;
+	while (cursor->more()) {
+		mongo::BSONObj b = cursor->next();
+		results[counter] = JSON_from_BSON(&b);
+		counter++;
+	}
+	return results;
 }
 
-bool MongoInterface::removeEntry(std::string collection, JsonBox::Value* data, bool onlyOne){
-	try{
-		connection->remove(database + "." + collection, BSON_from_JSON(data), onlyOne);
-	} catch(const mongo::DBException& e){
+bool MongoInterface::removeEntry(std::string collection, JsonBox::Value* data,
+		bool onlyOne) {
+	try {
+		connection->remove(database + "." + collection, BSON_from_JSON(data),
+				onlyOne);
+	} catch (const mongo::DBException& e) {
 		return false;
 	}
 	return true;
 }
 
 std::string MongoInterface::getDatabase() const {
-    return database;
+	return database;
 }
 
 void MongoInterface::setDatabase(std::string database) {
-    this->database = database;
+	this->database = database;
 }
 
 std::string MongoInterface::getIP_Port() const {
-    return IP_Port;
+	return IP_Port;
 }
--- MongoInterface.h
+++ MongoInterface.h
@@ -26,86 +26,88 @@
  */
 class MongoInterface {
 private:
-    std::string database;
-    std::string IP_Port;
-    mongo::DBClientConnection* connection;
-    /**
-     * Helper method to convert a JsonBox Value to a BSON object that MongoDB accepts
-     *
-     * @param data the JsonBox Value to convert
-     * @return the BSON object
-     */
-    mongo::BSONObj BSON_from_JSON(JsonBox::Value* data) const;
-    /**
-     * Helper method to convert a BSON object from MongoDB to a JsonBox Value
-     *
-     * @param data the BSON object to convert
-     * @return the JsonBox Value
-     */
-    JsonBox::Value JSON_from_BSON(mongo::BSONObj* data) const;
-    /**
-     * Helper method to connect to a specified database
-     *
-     * @param database The name of the database to connect to
-     * @param IP_Port The IP address and port given as a string in the form "IP:Port"
-     * @return true if the connection was successful
-     */
-    bool connect(std::string database, std::string IP_Port);
+	std::string database;
+	std::string IP_Port;
+	mongo::DBClientConnection* connection;
+	/**
+	 * Helper method to convert a JsonBox Value to a BSON object that MongoDB accepts
+	 *
+	 * @param data the JsonBox Value to convert
+	 * @return the BSON object
+	 */
+	mongo::BSONObj BSON_from_JSON(JsonBox::Value* data) const;
+	/**
+	 * Helper method to convert a BSON object from MongoDB to a JsonBox Value
+	 *
+	 * @param data the BSON object to convert
+	 * @return the JsonBox Value
+	 */
+	JsonBox::Value JSON_from_BSON(mongo::BSONObj* data) const;
+	/**
+	 * Helper method to connect to a specified database
+	 *
+	 * @param database The name of the database to connect to
+	 * @param IP_Port The IP address and port given as a string in the form "IP:Port"
+	 * @return true if the connection was successful
+	 */
+	bool connect(std::string database, std::string IP_Port);
 public:
-    /**
-     * Constructor opens connection to database with specified name.
-     *
-     * @param database Name of the database to connect to
-     * @param IP_Port The IP address and port given as a string in the form "IP:Port"
-     */
-    MongoInterface(std::string database="aqueti", std::string IP_Port="localhost");
-    /**
-     * Destructor
-     */
-    virtual ~MongoInterface();
-    /**
-     * Insert a JsonBox Value into the database with specified collection.
-     *
-     * @param collection The name of the collection to insert Value into
-     * @param data The JsonBox Value to insert
-     * @return true on success
-     */
-    bool insertJSON(std::string collection, JsonBox::Value* data) const;
-    /**
-     * Query the specified collection according to a specified JsonBox Value
-     *
-     * @param collection The name of the collection to query
-     * @param data The JsonBox Value query
-     * @return Array of results
-     */
-    JsonBox::Value query(std::string collection, JsonBox::Value* data) const;
-    /**
-     * Remove one or multiple entries from the specified collection
-     *
-     * @param collection The name of the collection
-     * @param data A JsonBox Value specifying what entries to remove
-     * @param onlyOne if true, a maximum of one entry will be removed
-     * @return true on success
-     */
-    bool removeEntry(std::string collection, JsonBox::Value* data, bool onlyOne);
-    /**
-     * Returns a string containing the name of the current database
-     *
-     * @return The name of the current database
-     */
-    std::string getDatabase() const;
-    /**
-     * Change the database to query
-     *
-     * @param database The name of the new database
-     */
-    void setDatabase(std::string database);
-    /**
-     * Returns a string containing the IP and Port of the database
-     *
-     * @return The IP and port in the form "IP:Port"
-     */
-    std::string getIP_Port() const;
+	/**
+	 * Constructor opens connection to database with specified name.
+	 *
+	 * @param database Name of the database to connect to
+	 * @param IP_Port The IP address and port given as a string in the form "IP:Port"
+	 */
+	MongoInterface(std::string database = "aqueti", std::string IP_Port =
+			"localhost");
+	/**
+	 * Destructor
+	 */
+	virtual ~MongoInterface();
+	/**
+	 * Insert a JsonBox Value into the database with specified collection.
+	 *
+	 * @param collection The name of the collection to insert Value into
+	 * @param data The JsonBox Value to insert
+	 * @return true on success
+	 */
+	bool insertJSON(std::string collection, JsonBox::Value* data) const;
+	/**
+	 * Query the specified collection according to a specified JsonBox Value
+	 *
+	 * @param collection The name of the collection to query
+	 * @param data The JsonBox Value query
+	 * @return Array of results
+	 */
+	JsonBox::Value query(std::string collection, JsonBox::Value* data) const;
+	/**
+	 * Remove one or multiple entries from the specified collection
+	 *
+	 * @param collection The name of the collection
+	 * @param data A JsonBox Value specifying what entries to remove
+	 * @param onlyOne if true, a maximum of one entry will be removed
+	 * @return true on success
+	 */
+	bool removeEntry(std::string collection, JsonBox::Value* data,
+			bool onlyOne);
+	/**
+	 * Returns a string containing the name of the current database
+	 *
+	 * @return The name of the current database
+	 */
+	std::string getDatabase() const;
+	/**
+	 * Change the database to query
+	 *
+	 * @param database The name of the new database
+	 */
+	void setDatabase(std::string database);
+	/**
+	 * Returns a string containing the IP and Port of the database
+	 *
+	 * @return The IP and port in the form "IP:Port"
+	 */
+	std::string getIP_Port() const;
 };
 
 bool mongoInterfaceTest();
