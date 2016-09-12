3f597bf01850b59c9d83be56d362996bbacf6115
Steve Feller <sfeller@aqueti.com>
2016-09-12 16:08:11 -0400

    "Mongo found!"

--- cmake/FindMongoC.cmake
+++ cmake/FindMongoC.cmake
@@ -4,23 +4,6 @@
 #
 include(FindPackageHandleStandardArgs)
 
-# if BSON_ROOT has defined in system environment, use this as a hint
-set(BSON_ROOT "$ENV{BSON_ROOT}")
-set(INSTALL_PATH "${CMAKE_BINARY_DIR}/INSTALL")
-set(MONGOC_ROOT "$ENV{MONGOC_ROOT}")
-
-#find the header files
-find_path(BSON_INCLUDE_DIRS "bson.h" HINTS 
-   "${BSON_ROOT}/include"
-   "${INSTALL_PATH}/include/libbson-1.0"
-)
-message( "INSTALL_PATH: ${INSTALL_PATH}")
-find_library(BSON_LIBRARY NAMES BSON HINTS 
-   "${BSON_ROOT}/lib"
-   "${INSTALL_PATH}/lib"
-)
-
-
 # if MONGOC_ROOT has defined in system environment, use this as a hint
 set(MONGOC_ROOT "$ENV{MONGOC_ROOT}")
 set(INSTALL_PATH "${CMAKE_BINARY_DIR}/INSTALL")
@@ -32,10 +15,9 @@ find_path(MONGOC_INCLUDE_DIRS "mongoc.h" HINTS
 )
 
 
-find_library(MONGOC_LIBRARY NAMES MONGOC HINTS 
+find_library(MONGOC_LIBRARY NAMES "mongoc-1.0" HINTS 
    "${MONGOC_ROOT}/lib"
    "${INSTALL_PATH}/lib"
 )
 #
-find_package_handle_standard_args(BSON DEFAULT_MSG BSON_INCLUDE_DIRS BSON_LIBRARY)
 find_package_handle_standard_args(MONGOC DEFAULT_MSG MONGOC_INCLUDE_DIRS MONGOC_LIBRARY)
