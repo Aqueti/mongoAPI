fc7d9247496a81df9d0635a9903f6a5e5c5dcda5
Erik Persson <npersson001@gmail.com>
2017-06-07 14:35:45 -0400

    updated cmakelists and FINALLY finished removing #define

--- mongoapi.h
+++ mongoapi.h
@@ -26,7 +26,7 @@
 #include <mongocxx/exception/bulk_write_exception.hpp>
 #include <assert.h>
 #include <aquetitools/Timer.h>
-//#include <mongoapi/revision.h>
+#include <mongoapi/revision.h>
 #include <aquetitools/revision.h>
 
 using bsoncxx::builder::stream::document;
