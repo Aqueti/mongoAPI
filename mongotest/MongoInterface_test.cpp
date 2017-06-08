298ef2e2d62ea0b01509d60ef0438cd93da8e3a2
Erik Persson <npersson001@gmail.com>
2017-06-08 16:03:12 -0400

    added option for command line execution

--- mongotest/MongoInterface_test.cpp
+++ mongotest/MongoInterface_test.cpp
@@ -7,17 +7,25 @@
 
 #include "mongoapi.h"
 
-int main() {
+int main(int argc, char *argv[]) {
 	//call unit test function and store result in JsonValue
 	std::string jsonString = mongoapi::testMongoInterface(true, false);
 	JsonBox::Value jsonVal;
 	jsonVal.loadFromString(jsonString);
 	std::cout << jsonVal << std::endl;
 
-	//connect to database and insert JsonValue
-	mongoapi::MongoInterface mi("aqueti");
-	mi.insertJSON("unit_tests", jsonVal);
+	//if the command line option is used then do not insert 
+	bool insert = true;
+	int i;
+	for(i = 0; i < argc; i++){
+		if(strcmp(argv[i], "-n") == 0){
+			insert = false;
+		}
+	}
 
-    
-
+	//connect to database and insert JsonValue if "-n" was not used
+	if(insert){
+		mongoapi::MongoInterface mi("aqueti");
+		mi.insertJSON("unit_tests", jsonVal);
+	}
 }
