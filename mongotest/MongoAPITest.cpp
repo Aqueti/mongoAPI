0389de013a615db27447321995759b503df2af21
Erik Persson <npersson001@gmail.com>
2017-06-16 16:36:24 -0400

    add testMongoAPI function changed structure of JSON creation

--- mongotest/MongoAPITest.cpp
+++ mongotest/MongoAPITest.cpp
@@ -7,11 +7,31 @@
 
 #include "mongoapi.h"
 
+/**
+ * \brief Run tests on all components in the mongoapi library
+ **/
+JsonBox::Value testMongoAPI( bool testSubmodules = true)
+{
+	JsonBox::Value value;
+    value["mongoInterface"] = testMongoInterface();
+
+    if( testSubmodules) {
+       value["submodule"]["AquetiTools"] = atl::testAquetiTools();
+    }
+
+    return value;
+}
+
+
 int main(int argc, char *argv[]) {
 	//call unit test function and store result in JsonValue
-	std::string jsonString = mongoapi::testMongoInterface(true, false);
-	JsonBox::Value jsonVal;
-	jsonVal.loadFromString(jsonString);
+//	std::string jsonString = mongoapi::testMongoInterface(true, false);
+//	JsonBox::Value jsonVal;
+//	jsonVal.loadFromString(jsonString);
+
+	jsonBox::Value result = testMongoAPI();
+
+
 
 	//if the command line option is used then do not insert 
 	bool insert = true;
@@ -31,6 +51,8 @@ int main(int argc, char *argv[]) {
 		}
 		else{
 			std::cout << "failed to insert unit test results" << std::endl;
+			return 0;
 		}
 	}
+
 }
