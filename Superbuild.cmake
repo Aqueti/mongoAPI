10fded15b6243fd11b2bc714af2ed59a15d446e2
Cameron Givler <cameron@aqueti.com>
2017-06-29 17:53:20 -0400

    working on cmake

--- Superbuild.cmake
+++ Superbuild.cmake
@@ -1,16 +1,22 @@
 set(cmake_common_args
-    -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-    -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
-    -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
-    -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
-    -DLIB_SUFFIX:STRING=${LIB_SUFFIX}
+    -DCMAKE_BUILD_TYPE:STRING=${CMAKE_BUILD_TYPE}
+    -DCMAKE_C_COMPILER:PATH=${CMAKE_C_COMPILER}
+    -DCMAKE_CXX_COMPILER:PATH=${CMAKE_CXX_COMPILER}
+    -DCMAKE_CXX_FLAGS:STRING=${CMAKE_CXX_FLAGS}
+    -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
     -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/INSTALL
     -DCMAKE_INSTALL_RPATH:PATH=${CMAKE_BINARY_DIR}/INSTALL/lib${LIB_SUFFIX}
-    -DUSE_SUPERBUILD:BOOL=FALSE
+    -DCMAKE_INCLUDE_PATH:PATH=${CMAKE_INSTALL_PREFIX}/include
+    -DLIB_SUFFIX:STRING=${LIB_SUFFIX}
+    -DMAKE_TESTS:BOOL=${MAKE_TESTS}
+    -DUSE_DOXYGEN:BOOL=${USE_DOXYGEN}
+    -DMAKE_DEB_PACKAGE:BOOL=${MAKE_DEB_PACKAGE}
+    -DUSE_SUPERBUILD:BOOL=OFF
 )
 
 # if MASTER is true, project will always be built. Else, only built if dependency
 macro(add_external_project MYNAME LOCATION MASTER DEPENDS ARGS)
+    message("${MYNAME} args: ${ARGS}")
     if(NOT ${MASTER})
         set(EXCLUDE ON)
     else()
@@ -23,16 +29,28 @@ macro(add_external_project MYNAME LOCATION MASTER DEPENDS ARGS)
         DOWNLOAD_COMMAND git submodule update --init ${CMAKE_SOURCE_DIR}/${LOCATION}
         DOWNLOAD_DIR ${CMAKE_SOURCE_DIR}
         CMAKE_ARGS ${cmake_common_args} ${ARGS}
-        INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+        INSTALL_DIR ${CMAKE_BINARY_DIR}/INSTALL
         DEPENDS ${DEPENDS}
     )
 endmacro(add_external_project)
 
-add_external_project(JsonBox dependencies/JsonBox OFF "" "")
+set(MONGOC_ARGS
+    -DENABLE_TESTS:BOOL=OFF
+    -DENABLE_EXAMPLES:BOOL=OFF
+)
+set(MONGOCXX_ARGS
+    -DLIBMONGOC_DIR=${CMAKE_INSTALL_PREFIX}
+    -DLIBBSON_DIR=${CMAKE_INSTALL_PREFIX}
+)
+set(BSON_ARGS
+    -DENABLE_TESTS:BOOL=OFF
+)
+
 add_external_project(libbson dependencies/libbson OFF "" "")
 add_external_project(MongoC dependencies/mongo-c-driver OFF "libbson" "")
-add_external_project(MongoCXX dependencies/mongo-cxx-driver OFF "MongoC" "")
-add_external_project(AquetiTools dependencies/AquetiTools OFF "" "")
+add_external_project(MongoCXX dependencies/mongo-cxx-driver OFF "MongoC;libbson" "${MONGOCXX_ARGS}")
+add_external_project(JsonBox dependencies/JsonBox OFF "" "")
+add_external_project(AquetiTools dependencies/AquetiTools OFF "JsonBox" "")
 
 ExternalProject_Add (
   mongoapi
