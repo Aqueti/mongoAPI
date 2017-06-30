4f4a2747150aa6e1069511c5733c4ba4d6c4df2e
Cameron Givler <cameron@aqueti.com>
2017-06-30 10:15:29 -0400

    builds

--- Superbuild.cmake
+++ Superbuild.cmake
@@ -6,7 +6,7 @@ set(cmake_common_args
     -DCMAKE_C_FLAGS:STRING=${CMAKE_C_FLAGS}
     -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_BINARY_DIR}/INSTALL
     -DCMAKE_INSTALL_RPATH:PATH=${CMAKE_BINARY_DIR}/INSTALL/lib${LIB_SUFFIX}
-    -DCMAKE_INCLUDE_PATH:PATH=${CMAKE_INSTALL_PREFIX}/include
+    -DCMAKE_INCLUDE_PATH:PATH=${CMAKE_BINARY_DIR}/INSTALL/include
     -DLIB_SUFFIX:STRING=${LIB_SUFFIX}
     -DMAKE_TESTS:BOOL=${MAKE_TESTS}
     -DUSE_DOXYGEN:BOOL=${USE_DOXYGEN}
@@ -16,7 +16,6 @@ set(cmake_common_args
 
 # if MASTER is true, project will always be built. Else, only built if dependency
 macro(add_external_project MYNAME LOCATION MASTER DEPENDS ARGS)
-    message("${MYNAME} args: ${ARGS}")
     if(NOT ${MASTER})
         set(EXCLUDE ON)
     else()
@@ -37,17 +36,19 @@ endmacro(add_external_project)
 set(MONGOC_ARGS
     -DENABLE_TESTS:BOOL=OFF
     -DENABLE_EXAMPLES:BOOL=OFF
+    -DENABLE_SASL:BOOL=OFF
 )
 set(MONGOCXX_ARGS
-    -DLIBMONGOC_DIR=${CMAKE_INSTALL_PREFIX}
-    -DLIBBSON_DIR=${CMAKE_INSTALL_PREFIX}
+    -DLIBMONGOC_DIR=${CMAKE_BINARY_DIR}/INSTALL
+    -DLIBBSON_DIR=${CMAKE_BINARY_DIR}/INSTALL
+    -DCMAKE_PREFIX_PATH:PATH=${CMAKE_BINARY_DIR}/INSTALL
 )
 set(BSON_ARGS
     -DENABLE_TESTS:BOOL=OFF
 )
 
-add_external_project(libbson dependencies/libbson OFF "" "")
-add_external_project(MongoC dependencies/mongo-c-driver OFF "libbson" "")
+add_external_project(libbson dependencies/libbson OFF "" "${BSON_ARGS}")
+add_external_project(MongoC dependencies/mongo-c-driver OFF "libbson" "${MONGOC_ARGS}")
 add_external_project(MongoCXX dependencies/mongo-cxx-driver OFF "MongoC;libbson" "${MONGOCXX_ARGS}")
 add_external_project(JsonBox dependencies/JsonBox OFF "" "")
 add_external_project(AquetiTools dependencies/AquetiTools OFF "JsonBox" "")
